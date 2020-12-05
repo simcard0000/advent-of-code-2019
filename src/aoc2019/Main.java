a67f4730a503f7081d0767b7c8c709ac2485f85c
simcard0000 <43153413+simcard0000@users.noreply.github.com>
2020-12-05 13:07:28 -0500

    Day 2 complete, added Day 1 and 2 inputs

--- src/aoc2019/Main.java
+++ src/aoc2019/Main.java
@@ -18,6 +18,7 @@ public class Main {
 				break;
 			}
 		}
+		in.close();
 		return answer;
 	}
 	
@@ -39,13 +40,89 @@ public class Main {
 				break;
 			}
 		}
+		in.close();
+		return answer;
+	}
+	
+	static int day2A() {
+		int answer = 0;
+		String line = in.nextLine();
+		//taking input line and making an array out of it:
+		in.close();
+		String[] codes = line.split(",");
+		codes[1] = Integer.toString(12);
+		codes[2] = Integer.toString(2);
+		for (int i = 0; i < codes.length; i += 4) {
+			if (codes[i].equals("1") || codes[i].equals("2")) {
+				int outputPos = Integer.valueOf(codes[i+3]).intValue();
+				int arg1Pos = Integer.valueOf(codes[i+1]).intValue();
+				int arg2Pos = Integer.valueOf(codes[i+2]).intValue();
+				int arg1 = Integer.valueOf(codes[arg1Pos]).intValue();
+				int arg2 = Integer.valueOf(codes[arg2Pos]).intValue();
+				if (codes[i].equals("1")) {
+					codes[outputPos] = Integer.toString(arg1 + arg2);
+				} else {
+					codes[outputPos] = Integer.toString(arg1 * arg2);
+				}
+			} else if (codes[i].equals("99")) {
+				break;
+			}
+		}
+		answer = Integer.valueOf(codes[0]).intValue();
+		return answer;
+	}
+	
+	static int day2B() {
+		int answer = 0;
+		String line = in.nextLine();
+		//taking input line and making an array out of it:
+		in.close();
+		String[] codes = line.split(",");
+		String[] originalCodes = codes.clone();
+		boolean foundAns = false;
+		//finding the right inputs:
+		for (int i = 0; i < 100; i++) {
+			codes[1] = Integer.toString(i);
+			for (int j = i + 1; j < 100; j++) {
+				codes[2] = Integer.toString(j);
+				for (int k = 0; k < codes.length; k +=4) {
+					if (codes[k].equals("1") || codes[k].equals("2")) {
+						int outputPos = Integer.valueOf(codes[k+3]).intValue();
+						int arg1Pos = Integer.valueOf(codes[k+1]).intValue();
+						int arg2Pos = Integer.valueOf(codes[k+2]).intValue();
+						int arg1 = Integer.valueOf(codes[arg1Pos]).intValue();
+						int arg2 = Integer.valueOf(codes[arg2Pos]).intValue();
+						if (codes[k].equals("1")) {
+							codes[outputPos] = Integer.toString(arg1 + arg2);
+						} else {
+							codes[outputPos] = Integer.toString(arg1 * arg2);
+						}
+					} else if (codes[k].equals("99")) {
+						break;
+					}
+				}
+				if (Integer.valueOf(codes[0]).intValue() == 19690720) {
+					foundAns = true;
+					answer = 100 * i + j;
+					break;
+				} 
+				codes = originalCodes.clone();
+				codes[1] = Integer.toString(i);
+			}
+			if (foundAns) {
+				break;
+			}
+		}
 		return answer;
 	}
 
 	public static void main(String[] args) {
+		//Comment out the respective print statement!
 		
 		//System.out.println("day1A answer: " + day1A());
 		//System.out.println("day1B answer: " + day1B());
+		//System.out.println("day2A answer: " + day2A());
+		//System.out.println("day2B answer: " + day2B());
 	}
 
 }
